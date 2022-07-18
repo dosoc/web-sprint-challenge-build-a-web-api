e4e7b6263226a3d8fc41aad881bdbfef41572255
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-17 20:54:48 -0500

    adds delete actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -28,11 +28,23 @@ actionsRouter.post('/', validateAction, (req, res, next) => {
     })
     .catch(next)
 })
-actionsRouter.put('/:id', (req, res) => {
-
+actionsRouter.put('/:id', validateActionId, validateAction, (req, res, next) => {
+    Actions.update(req.params.id, req.body)
+    .then(()=> {
+        return Actions.get(req.params.id)
+    })
+    .then(updatedAction => {
+        res.status(201).json(updatedAction)
+    })
+    .catch(next)
 })
-actionsRouter.delete('/:id', (req, res) => {
-
+actionsRouter.delete('/:id', validateActionId, async (req, res) => {
+    try{
+        await Actions.remove(req.params.id)
+        res.json(req.user)
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = actionsRouter
